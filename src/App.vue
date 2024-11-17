e7737cead694de385af793d6d3630ec5d8267671
thomas.margueritat38 <thomas.margueritat38@gmail.com>
2024-11-17 22:30:45 +0000

    add flyonUI with doc example

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
-<template>
-  <h1 class="text-3xl font-bold underline">
-    I started a new version of the website yet again.
-  </h1>
+<template><button
+  type="button"
+  class="collapse-toggle btn btn-primary"
+  id="basic-collapse"
+  aria-expanded="false"
+  aria-controls="basic-collapse-heading"
+  data-collapse="#basic-collapse-heading"
+>
+  <span class="collapse-open:hidden">Collapsed</span>
+  <span class="collapse-open:block hidden">Collapse</span>
+  <span class="icon-[tabler--chevron-down] collapse-open:rotate-180 transition-rotate size-4 duration-300"></span>
+</button>
+<div
+  id="basic-collapse-heading"
+  class="collapse hidden w-full overflow-hidden transition-[height] duration-300"
+  aria-labelledby="basic-collapse"
+>
+  <div class="border-base-content/25 mt-3 rounded-md border p-3">
+    <p class="text-base-content/80">
+      The collapsible body remains hidden until the collapse plugin adds specific classes. These control appearance and
+      manage visibility through transitions.
+    </p>
+  </div>
+</div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flyonui/dist/js/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flyonui"),
+    require("flyonui/plugin")
+  ],
+  flyonui: {
+    themes: "dark",
+    darkTheme: "dark",
+  }
 }
