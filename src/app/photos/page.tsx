b50fac96557abac72f3f9426b39a3d232920ff33
thomas.margueritat38 <thomas@marguerit.at>
2024-12-22 11:20:53 +0000

    fix loading issue with Images

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -4,7 +4,7 @@ import Social from "@/components/Social";
 import Image from 'next/image'
 
 import { ListBlobResultBlob } from '@vercel/blob';
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getBlobs } from "./actions";
 
 export default function Photos() {
@@ -15,31 +15,24 @@ export default function Photos() {
   const [numberOfPages, setNumberOfPages] = useState(1)
   const [buttonRow, setButtonRow] = useState(Array<unknown>)
   const [currentPage, setCurrentPage] = useState(1)
-  const [numberOfLoadedPhotos, setNumberOfPhotosLoaded] = useState(0)
+  const [urlArray, setUrlArray] = useState(["", "", "", ""])
 
   function setPage(page: number): void {
-    setCurrentPage(page)
+    setCurrentPage(page) 
+    setUrlArray([blobs[4 * (page - 1) + 1].url, blobs[4 * (page - 1) + 2].url, blobs[4 * (page - 1) + 3].url, blobs[4 * (page - 1) + 4].url])
   }
 
-  function addOneLoadedPhoto(): void {
-    setNumberOfPhotosLoaded((prev) => prev + 1);
-  }
-
-  function removeNumberofLoadedPhoto(): void {
-    setNumberOfPhotosLoaded(0)
-  }
-  
   useEffect(() => {
   	getBlobs().then(response => {
       setBlobs(response.blobs)
       // need to substract 1 from the blobs length because blob folder itself is included in response 
       setNumberOfPages(Math.ceil((response.blobs.length - 1) / numberOfPhotosByPage))
+      setUrlArray([response.blobs[currentPage].url, response.blobs[currentPage + 1].url, response.blobs[currentPage + 2].url, response.blobs[currentPage + 3].url])
     })
   }, []);
 
   useEffect(() => {
     setButtonRow([])
-    removeNumberofLoadedPhoto()
     for (let i = 1; i <= numberOfPages; i++) {
       setButtonRow(prevButtonRow => [...prevButtonRow, <button type="button" key={i} aria-current={i === currentPage ? 'page' : false} className="btn btn-soft btn-square aria-[current='page']:text-bg-soft-primary" onClick={() => setPage(i)}>{i}</button>])
     }
@@ -49,16 +42,24 @@ export default function Photos() {
 const CustomImage = (props) => {
   const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    setIsLoading(true)
+  }, [props.srcImage]);
+
   return (
-    <div>
-      {isLoading && <div className={`skeleton h-72 w-72 animate-pulse`} />}
-      <Image
-        {...props}
-        alt="photos"
-        width={288}
-        height={288}
-        onLoad={() => setIsLoading(false)}
-      />
+    <div className="h-72">
+      { (props.srcImage === "" || isLoading ) && <div className={`skeleton h-72 w-72 animate-pulse`} /> }
+      { props.srcImage !== "" && 
+        <Image
+          src={props.srcImage}
+          alt="photos"
+          width={288}
+          height={288}
+          priority
+          onLoad={() => setIsLoading(false)}
+          className={`${isLoading && "hidden"}`}
+        />
+      }
     </div>
   );
 };
@@ -82,16 +83,13 @@ const CustomImage = (props) => {
       </div>
       <div className="mt-10 mb-5 max-w-4xl">
         <div className={`flex flex-row flex-wrap gap-5 items-center justify-center`}>
-          {blobs
-            .filter(blob => blob.size !== 0)
-            .slice(numberOfPhotosByPage * (currentPage - 1), numberOfPhotosByPage * currentPage)
-            .map((blob, index) => (
-              <div key={index}>
-                <CustomImage src={blob.url} />
-              </div>
-          ))} 
+            <CustomImage srcImage={urlArray[0]} />
+            <CustomImage srcImage={urlArray[1]} />
+            <CustomImage srcImage={urlArray[2]} />
+            <CustomImage srcImage={urlArray[3]} />
         </div>
           <nav className="flex items-center justify-center gap-2 mt-8 mb-1">
+
             <button type="button" className={`btn btn-soft ${currentPage === 1 && "btn-disabled"}`} onClick={() => setPage(currentPage - 1)}>Previous</button>
             {buttonRow}
             <button type="button" className={`btn btn-soft ${currentPage === numberOfPages && "btn-disabled"}`} onClick={() => setPage(currentPage + 1)}>Next</button>
