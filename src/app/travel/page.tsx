68c53e41c2c2d42d92a0f322046c1930459a93b1
thomas.margueritat38 <thomas@marguerit.at>
2024-12-25 22:11:15 +0000

    improve map system

--- src/app/travel/page.tsx
+++ src/app/travel/page.tsx
@@ -4,7 +4,15 @@ import dynamic from "next/dynamic";
 
 export default function Travel() {
     function MapSkeleton() {
-        return <div className={`skeleton h-96 w-auto animate-pulse`} />
+        return (
+            <div>
+                <div className="mb-7 flex justify-center items-center gap-x-3">  
+                    <div className={`skeleton h-10 w-28	animate-pulse`} />
+                    <div className={`skeleton h-10 w-28	animate-pulse`} />
+                </div>
+                <div className={`skeleton h-96 w-auto animate-pulse`} />
+            </div>
+        )
     } 
 
     const Map = dynamic(() => import("@/components/Map"), { ssr: false, loading: () => MapSkeleton() });
@@ -14,7 +22,7 @@ export default function Travel() {
             <div className="w-full px-4 max-w-xl">
                 <div className="text-left">
                     <h1 className="text-2xl font-bold">Travel</h1>
-                    <p className="text-lg">What is life without travel?</p>
+                    <p className="text-lg">My love for travel and cartography on a single page.</p>
                 </div>
                 <div className="mt-10">
                     <Map />
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,51 +1,166 @@
-import { useEffect, useRef, useState } from "react";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import { useEffect, useRef, useState } from "react"
+import L from "leaflet"
+import "leaflet/dist/leaflet.css"
 import points from "./points.json"
+import cities from "./cities.json"
 
-const defaultPosition: L.LatLngExpression = [45.50556252099984, -73.60305089558513]
+const defaultPosition: L.LatLngExpression = [0, 0]
 const defaultZoom = 3
 
-const iconMap: { [key: string]: string } = {
-  "walk": "fa-hiking",
-  "photo": "fa-camera",
-  "building": "fa-building",
-  "urbex": "fa-ghost",
-  "art": "fa-paintbrush",
-  "station": "fa-train",
-  "food": "fa-utensils",
-  "lighthouse": "fa-lightbulb",
-  "port": "fa-anchor",
-  "museum": "fa-building-columns",
-  "park": "fa-tree-city",
-  "monument": "fa-monument",
-  "religion": "fa-cross",
-  "national-park": "fa-mountain",
-  "water": "fa-water",
-  "tunnel": "fa-car-tunnel",
-  "airport": "fa-plane-departure",
-  "castle": "fa-chess-rock",
-  "bridge": "fa-bridge",
-  "tower": "fa-gopuram",
-  "cave": "fa-icicles",
-  "boat": "fa-ship",
-  "street": "fa-signs-post",
-  "plane": "fa-jet-fighter-up",
-  "train": "fa-train",
-  "capitol": "fa-landmark-flag",
-  "library": "fa-book",
-  "text": "fa-comment",
-  "sailbot": "fa-sailbot",
-  "telepherique": "fa-cable-car",
-  "heliport": "fa-helicopter",
-  "beach": "fa-umbrella-beach",
-  "stairs": "fa-stairs"
+interface IconMap {
+  [key: string]: {
+    icon: string, 
+    text: string
+  }
+}
+
+interface Geolocalisation {
+  latitude: number,
+  longitude: number,
+  isApproximated: boolean
+}
+
+interface Point {
+  name: string,
+  type: string,
+  geolocalisation: Array<Geolocalisation>
+}
+
+type Mode = 'location' | 'city'
+
+const iconMap: IconMap = {
+  "walk": {
+    "icon": "fa-hiking",
+    "text": "Walk, hike"
+  },
+  "photo": {
+    "icon": "fa-camera",
+    "text": "Viewpoint, photo spot, belvedere"
+  },
+  "building": {
+    "icon": "fa-building",
+    "text": "Building, hall, mall"
+  },
+  "urbex": {
+    "icon": "fa-ghost",
+    "text": "Exploration, abandoned place/object"
+  },
+  "art": {
+    "icon": "fa-paintbrush",
+    "text": "Works of art"
+  },
+  "station": {
+    "icon": "fa-train",
+    "text": "Train station, train"
+  },
+  "food": {
+    "icon": "fa-utensils",
+    "text": "Restaurant, bakery, shop"
+  },
+  "lighthouse": {
+    "icon": "fa-lightbulb",
+    "text": "Lighthouse"
+  },
+  "port": {
+    "icon": "fa-anchor",
+    "text": "Port"
+  },
+  "museum": {
+    "icon": "fa-building-columns",
+    "text": "Museum"
+  },
+  "park": {
+    "icon": "fa-tree-city",
+    "text": "Park, garden, special area"
+  },
+  "monument": {
+    "icon": "fa-monument",
+    "text": "Monument, memoriel"
+  },
+  "religion": {
+    "icon": "fa-religion",
+    "text": "Church, cross"
+  },
+  "national-park": {
+    "icon": "fa-mountain",
+    "text": "National park, provincial park, state park, park"
+  },
+  "water": {
+    "icon": "fa-water",
+    "text": "Falls, lake, pond, river"
+  },
+  "tunnel": {
+    "icon": "fa-car-tunnel",
+    "text": "Tunnel, car tunnel, train tunnel"
+  },
+  "airport": {
+    "icon": "fa-plane-departure",
+    "text": "Airport, air show"
+  },
+  "castle": {
+    "icon": "fa-chess-rook",
+    "text": "Castle"
+  },
+  "bridge": {
+    "icon": "fa-bridge",
+    "text": "Bridge"
+  },
+  "cave": {
+    "icon": "fa-icicles",
+    "text": "Cave, mine"
+  },
+  "boat": {
+    "icon": "fa-ship",
+    "text": "Boat"
+  },
+  "street": {
+    "icon": "fa-signs-post",
+    "text": "Street, alley"
+  },
+  "plane": {
+    "icon": "fa-plane",
+    "text": "Plane"
+  },
+  "train": {
+    "icon": "fa-train",
+    "text": "Train"
+  },
+  "capitol": {
+    "icon": "fa-landmark-flag",
+    "text": "Capitol, state capitol"
+  },
+  "library": {
+    "icon": "fa-book",
+    "text": "Library"
+  },
+  "text": {
+    "icon": "fa-comment",
+    "text": "Sign"
+  },
+  "sailbot": {
+    "icon": "fa-sailbot",
+    "text": "Sailtbot"
+  },
+  "telepherique": {
+    "icon": "fa-cable-car",
+    "text": "Telepherique"
+  },
+  "heliport": {
+    "icon": "fa-helicopter",
+    "text": "Heliport, helicopter"
+  },
+  "beach": {
+    "icon": "fa-umbrella-beach",
+    "text": "Beach"
+  },
+  "stairs": {
+    "icon": "fa-stairs",
+    "text": "Stairs"
+  }
 }
 
 export default function Map() {
   const mapContainer = useRef(null);
-
-  type Mode = 'location' | 'city'
 
   const [mode, setMode] = useState<Mode>('location')
 
@@ -56,23 +171,39 @@ export default function Map() {
   useEffect(() => {
     if (!mapContainer.current) return;
 
-    // Initialize the map
     const map = L.map(mapContainer.current).setView(defaultPosition, defaultZoom);
 
     function getIcon(type: string): L.DivIcon {
       return L.divIcon({
-        html: `<div class='marker-pin'></div><i class='fa ${iconMap[type] || "fa-circle"} awesome'>`,
+        html: `<div class='marker-pin'></div><i class='fa ${iconMap?.[type]?.icon || "fa-circle"} awesome'>`,
         iconSize: [30, 42],
         iconAnchor: [15, 42],
         className: 'custom-div-icon'
       })
     }
 
+    function getDescription(point: Point, geo: Geolocalisation): string {
+      let description = `<b>${point.name}</b><br>`
+      description += `<u>Category</u>: ${iconMap?.[point.type]?.text || "Place, uncategorized"}`
+      if (geo.isApproximated) description += `<br /><br/><i>Position purposely approximated</i>`
+      return description
+    }
+
     const marketGroup = []
 
-    for (const point of points) {
-      for (const geo of point.geolocalisation) {
-        const marker = L.marker([geo.latitude, geo.longitude], { icon: getIcon(point.type)})
+    if (mode === 'location') {
+      for (const point of points) {
+        for (const geo of point.geolocalisation) {
+          const marker = L.marker([geo.latitude, geo.longitude], { icon: getIcon(point.type)})
+          marker.bindPopup(getDescription(point, geo)).openPopup();
+          marketGroup.push(marker)
+          marker.addTo(map)
+        }
+      }
+    } else {
+      for (const city of cities) {
+        const marker = L.marker([city.geolocalisation.latitude, city.geolocalisation.longitude], { icon: getIcon("") })
+        marker.bindPopup(`<b>${city.name}</b>`).openPopup();
         marketGroup.push(marker)
         marker.addTo(map)
       }
@@ -80,16 +211,14 @@ export default function Map() {
 
     map.fitBounds(new L.featureGroup(marketGroup).getBounds())
 
-    // Add a tile layer
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap",
-    }).addTo(map);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution: "© OpenStreetMap" }).addTo(map);
 
 
     return () => {
       map.remove()
     };
-  }, []);
+  }, [mode]);
 
   return (
     <div>
